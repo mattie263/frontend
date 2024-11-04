2f43666bbda6ed1e7835031d89f721403ee25d38
Connor Silvester <connorsilvester23@gmail.com>
2024-11-04 14:05:12 +0000

    Initial project setup.

---
+++ src/test/java/uk/ac/sheffield/team10/Team10ApplicationTests.java
@@ -0,0 +1,12 @@
+package uk.ac.sheffield.team10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team10ApplicationTests {
+
+    // @Test
+    // void contextLoads() {
+    // }
+}
